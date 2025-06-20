5c152bdafeb54ded6f878fbee3dab7227ce75a35
Josh Yeaton <yeaton.josh@gmail.com>
2025-06-20 10:39:20 -0400

    Add gameboard class

---
+++ javascript.js
@@ -0,0 +1,50 @@
+const Gameboard = (function () {
+    // 0-2 row 1, 3-5 row 2, 6-8 row 3
+    const gameState = ["", "", "", "", "", "", "", "", ""];
+
+    // Return true on a valid selection, false otherwise
+    const pickSquare = function(marker, loc) {
+        if (gameState[loc] === "") {
+            gameState[loc] = marker;
+            return true;
+        }
+        return false;
+    }
+
+    const checkWin = function() {
+        // Condition 1: a player has gotten a row/column/diag
+        // Check rows/cols
+        for (let i = 0; i < 3; i += 1) {
+            // rows
+            if ((gameState[3*i] != "") && (gameState[3*i] === gameState[3*i + 1]) && (gameState[3*i] === gameState[3*i + 2])) {
+                return gameState[3*i];
+            }
+            //cols
+            if ((gameState[i] != "") && (gameState[i] === gameState[i + 3]) && (gameState[i] === gameState[i + 6])) {
+                return gameState[i];
+            }
+        }
+        // Check diags
+        if ((gameState[0] != "") && (gameState[0] === gameState[4]) && (gameState[0] === gameState[8])) {
+            return gameState[0];
+        }
+        if ((gameState[2] != "") && (gameState[2] === gameState[4]) && (gameState[2] === gameState[6])) {
+            return gameState[2];
+        }
+        // Condition 2: all squares filled with no winner
+        if (gameState.includes("")) {
+            return "";
+        }
+        return "NONE";
+    }
+
+    const resetBoard = function() {
+        for (let i = 0; i < gameState.length; i++) {
+            gameState[i] = "";
+        }
+    }
+
+    const getBoard = () => gameState;
+
+    return {pickSquare, checkWin, resetBoard, getBoard};
+})();
